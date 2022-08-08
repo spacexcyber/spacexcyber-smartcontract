e2f09d330c5fe1732f43e24e370c6c2b64591071
athenaic <athenaic.offical@gmail.com>
2022-08-08 09:52:13 +0700

    add contract

---
+++ contracts/SpaceXCyber.sol
@@ -0,0 +1,10 @@
+// contracts/SpaceXCyber.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SpaceXCyber is ERC1155, ERC721 {
+    
+}
